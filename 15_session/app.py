46fe1ab0f96b8a7a335616a6834578934b251efc
Christopher Liu <christopherliu@sorcero.com>
2021-10-18 08:42:31 -0400

    Add response for logged in user and logout endpoint

--- 15_session/app.py
+++ 15_session/app.py
@@ -3,15 +3,22 @@
 # K15 -- Sessions Greetings
 # 2021-10-18
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
+
+# Secret key for session
+app.secret_key = "SUPER_SECRET_OOOH_AHAHAH"
 
 
 @app.route("/", methods=["GET"])
 def index():
     """Displays a login page if the user is not logged in and a welcome page
     if the user is logged in."""
+
+    if "username" in session:
+        # User is logged in
+        return render_template("response.html", username=session["username"])
 
     return render_template("login.html")
 
@@ -32,10 +39,19 @@ def auth():
 
     if username == correct_username and password == correct_password:
         # Authentication success
-        return render_template("response.html", username=request.form["username"])
+        session["username"] = username
+        return redirect(url_for("index"))
     else:
         # Authentication failure
         return render_template("error.html")
+
+
+@app.route("/logout")
+def logout():
+    """Logs out the user and returns to the log in page."""
+    if "username" in session:
+        del session["username"]
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
