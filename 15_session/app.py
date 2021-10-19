8328c51319c2fb8029ac160e66ea6fc8e91a7f7e
Christopher Liu <christopherliu@sorcero.com>
2021-10-19 08:19:49 -0400

    Return specific authentication error messages

--- 15_session/app.py
+++ 15_session/app.py
@@ -3,12 +3,14 @@
 # K15 -- Sessions Greetings
 # 2021-10-18
 
+from os import urandom
+
 from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
 
-# Secret key for session
-app.secret_key = "SUPER_SECRET_OOOH_AHAHAH"
+# Secret key for session (32 random bytes)
+app.secret_key = urandom(32)
 
 
 @app.route("/", methods=["GET"])
@@ -28,6 +30,10 @@ def auth():
     """Authenticates a log in request and either logs in or returns an error
     page."""
 
+    # Redirect to root path if GET request
+    if request.method == "GET":
+        return redirect(url_for("index"))
+
     # XXX HARDCODED USERNAME AND PASSWORD
     correct_username = "circletable"
     correct_password = "123456"
@@ -36,14 +42,19 @@ def auth():
     username = request.form.get("username", default="")
     password = request.form.get("password", default="")
 
-
     if username == correct_username and password == correct_password:
         # Authentication success
         session["username"] = username
         return redirect(url_for("index"))
+    elif username == correct_username:
+        # Authentication failure -- wrong password
+        return render_template("error.html", errors="password")
+    elif password == correct_password:
+        # Authentication failure -- wrong username
+        return render_template("error.html", errors="username")
     else:
-        # Authentication failure
-        return render_template("error.html")
+        # Authentication failure -- wrong both
+        return render_template("error.html", errors="username, password")
 
 
 @app.route("/logout")
